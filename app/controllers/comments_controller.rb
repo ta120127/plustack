604da73eb6f91bc6ec3cc3be59ff5d508ee86ec4
川口雄大 <take@kawaguchiyuudainoMacBook-Pro.local>
2021-03-08 18:08:08 +0900

    コメントのフラッシュメッセージ&タブ機能の不具合修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,10 @@ class CommentsController < ApplicationController
     comment = post.comments.build(comment_params) #buildを使い、contentとpost_idの二つを同時に代入
     comment.user_id = current_user.id
     if comment.save
-      flash[:success] = "コメントしました"
+      flash[:notice] = "コメントしました"
       redirect_back(fallback_location: root_path)
     else
-      flash[:success] = "コメントできませんでした"
+      flash[:alert] = "コメントできませんでした"
       redirect_back(fallback_location: root_path)
     end
   end
